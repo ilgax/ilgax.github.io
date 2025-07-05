051460628ffd3945702d59bc977e73c227e19b56
Ilgaz <notilgax@pm.me>
2025-07-05 17:48:10 +0300

    svelte time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ilgax.github.io',
+  site: 'https://ilgax.github.io',
+  integrations: [svelte()],
 });
